eccf7e8654a12bed75d5543d5cc5b50950b83b40
Sanghyeok <60688266+LSanghyeok@users.noreply.github.com>
2020-06-11 15:27:09 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt\
+import matplotlib.pyplot as plt
 
 idx2label = {0:"bathhub",
              1:"bed",
@@ -11,12 +11,10 @@ idx2label = {0:"bathhub",
              8:"table",
              9:"toilet"}
 
-def visualize(label, X):
-    label=label.item()
-    
+def visualize(pos,label):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:, 0],  X[:, 1], X[:, 2], color='red', s=5)
+    ax.scatter(pos[:, 0], pos[:, 1], pos[:, 2], color='red', s=5)
     ax.text2D(0.87, 0.92, 'Label: {}'.format(
         idx2label[label]), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
     plt.draw()
